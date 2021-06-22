59c73e8b28cfb5a74c4e560335a456593ab46082
Josiah Berkebile <praenato14@gmail.com>
2021-06-22 11:45:13 -0500

    make username and email parameterized

--- 27.2/ubuntu/20.04/config.el
+++ 27.2/ubuntu/20.04/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "Josiah Berkebile"
-      user-mail-address "josiah.berkebile@protonmail.com")
+(setq user-full-name <full-name>
+      user-mail-address <email-address>)
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
