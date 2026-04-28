cba54df06e908d1a96fe8b0764b23938bfb290ae
Josiah Berkebile <praenato14@gmail.com>
2026-04-28 13:00:53 -0500

    fix build script, correct ncurses policy vs install mismatch

--- 29.2/ubuntu/24.04/x86_64/dev/build.sh
+++ 29.2/ubuntu/24.04/x86_64/dev/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build . -t josiah14/emacs:29.2-skylake-ubuntu-24.04-dev \                ❮❮❮
+docker build . -t josiah14/emacs:29.2-skylake-ubuntu-24.04-dev \
   --build-arg USERNAME="${USER}" \
   --build-arg UID="${UID}" --build-arg GID="${GID}"
