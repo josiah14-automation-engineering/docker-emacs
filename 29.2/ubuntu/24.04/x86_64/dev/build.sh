06da33e4564a533f5cccc4b66264fad8e02695b2
Josiah Berkebile <praenato14@gmail.com>
2026-04-28 13:10:21 -0500

    fix user creation issues and edge-cases

--- 29.2/ubuntu/24.04/x86_64/dev/build.sh
+++ 29.2/ubuntu/24.04/x86_64/dev/build.sh
@@ -2,4 +2,4 @@
 
 docker build . -t josiah14/emacs:29.2-skylake-ubuntu-24.04-dev \
   --build-arg USERNAME="${USER}" \
-  --build-arg UID="${UID}" --build-arg GID="${GID}"
+  --build-arg USER_UID="$(id -u)" --build-arg USER_GID="$(id -g)"
