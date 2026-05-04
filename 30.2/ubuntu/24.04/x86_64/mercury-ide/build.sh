aa4eba9b0288d930dc947ebd9538c94cb5fb2831
Josiah Berkebile <praenato14@gmail.com>
2026-05-04 11:27:13 -0500

    Create Doom base with included Mercury tooling for Mercury IDE

--- 30.2/ubuntu/24.04/x86_64/mercury-ide/build.sh
+++ 30.2/ubuntu/24.04/x86_64/mercury-ide/build.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-docker build . --ulimit nofile=262144:262144 -t josiah14/mercury-doom-emacs-ide:29.2-skylake-ubuntu-22.04 \
-  --build-arg username="${USER}" \
-  --build-arg uid="${UID}" \
-  --build-arg guid="${GID}" \
-  --build-arg fullname="Josiah Berkebile" \
-  --build-arg email="josiah.berkebile@protonmail.com"
+docker build . \
+  --ulimit nofile=262144:262144 \
+  -t josiah14/mercury-doom-emacs-ide:30.2-skylake-ubuntu-24.04 \
+  --build-arg USERNAME="${USER}" \
+  --build-arg USER_UID="$(id -u)" \
+  --build-arg USER_GID="$(id -g)" \
+  --build-arg FULLNAME="Josiah Berkebile" \
+  --build-arg EMAIL="josiah.berkebile@protonmail.com"
